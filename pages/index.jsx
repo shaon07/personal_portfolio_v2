44afa298137813a3ec6e6955f39d2f6d1fe60467
SHAON <mdshamirulislam@SHAONs-MacBook-Air.local>
2024-02-29 19:58:35 +0600

    [update] layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,21 +14,34 @@ export default function HomePage() {
           <div className={styles.content}>
             <h1 className={styles.name}>SHAMIRUL ISLAM</h1>
             <h6 className={styles.bio}>Frontend Developer</h6>
-            <Link href="/projects">
-              <button className={styles.button}>View Work</button>
-            </Link>
-            <Link href="/contact">
-              <button className={styles.outlined}>Contact Me</button>
-            </Link>
-            <Link href="https://drive.google.com/file/d/1rmyaWqfeFWDCF_7pKBcluA-Kg9tscLDK/view?usp=drive_link" target="_blank">
-              <button style={{marginLeft:"20px"}} className={styles.button}>Download CV</button>
-            </Link>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "20px",
+                flexWrap: "wrap",
+                position: "relative",
+                zIndex: "999",
+              }}
+            >
+              <Link href="/projects">
+                <button className={styles.button}>View Work</button>
+              </Link>
+              <Link href="/contact">
+                <button className={styles.outlined}>Contact Me</button>
+              </Link>
+              <Link
+                href="https://drive.google.com/file/d/1rmyaWqfeFWDCF_7pKBcluA-Kg9tscLDK/view?usp=drive_link"
+                target="_blank"
+              >
+                <button className={styles.button}>Resume</button>
+              </Link>
+            </div>
           </div>
           <Illustration className={styles.illustration} />
         </div>
       </div>
-
-   
     </>
   );
 }
